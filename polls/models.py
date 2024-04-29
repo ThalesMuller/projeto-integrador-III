48a46d177c1d6cd434ee4b20af88d94d684eb142
ebmeurer <38538609+ebmeurer@users.noreply.github.com>
2024-04-29 14:04:47 +0000

    fix: change ids to autofields

--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Livro(models.Model):
-    id = models.IntegerField()
+    id = models.AutoField(primary_key=True)
     isbn = models.CharField(max_length=13)
     titulo = models.CharField(max_length=100)
     genero = models.CharField(max_length=50)
@@ -15,7 +15,7 @@ class Livro(models.Model):
     created_by = models.IntegerField()
 
 class Usuario(models.Model):
-    id = models.IntegerField()
+    id = models.AutoField(primary_key=True)
     login = models.CharField(max_length=20)
     senha = models.CharField(max_length=64)
     email = models.CharField(max_length=100)
@@ -23,7 +23,7 @@ class Usuario(models.Model):
     created_by = models.IntegerField()
 
 class Resenha(models.Model):
-    id = models.IntegerField()
+    id = models.AutoField(primary_key=True)
     cod_livro = models.IntegerField()
     texto = models.TextField()
     rating = models.FloatField()
@@ -31,7 +31,7 @@ class Resenha(models.Model):
     created_by = models.IntegerField()
 
 class Comentario(models.Model):
-    id = models.IntegerField()
+    id = models.AutoField(primary_key=True)
     cod_resenha = models.IntegerField()
     texto = models.TextField()
     created_at = models.DateField()
