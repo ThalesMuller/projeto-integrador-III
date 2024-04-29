7c2803daf5a856dc4ad8875c0129d696b01ea132
Thales_Müller <Thales.em@gmail.com>
2024-04-29 15:41:29 +0000

    refactor: change database

--- margm/settings.py
+++ margm/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'margm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MARGM_DB',
+        'USER': 'seu_usuario',
+        'PASSWORD': 'sua_senha',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
