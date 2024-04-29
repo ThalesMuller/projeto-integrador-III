be9ac544a8d38906d4ac973fddd41d41fbd38f98
ebmeurer <38538609+ebmeurer@users.noreply.github.com>
2024-04-29 14:47:41 +0000

    feat: add views + migration

--- polls/urls.py
+++ polls/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index"),]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path('livros/', views.get_livros, name='get_livros'),
+    ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from .forms import LivroForm
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def get_livros(request):
+    obj = Livro.objects.all()
+    template_name = 'null'
+    context = {'obj': obj}
+    return render(request, template_name, context)
